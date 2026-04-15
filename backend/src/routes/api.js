b179b68cf6c561d87e00eb563cf56add24ab9ce3
Khai Nguyen <khai.nguyen03042005@hcmut.edu.vn>
2026-04-15 17:32:34 +0700

    jwt

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,28 +1,18 @@
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 const {
-    getUsersAPI,
-    postCreateUserAPI,
     postEnrollUserAPI,
-    putUpdateUserAPI,
-    deleteUserAPI
 } = require('../controllers/adminController');
 const { handleLogin } = require('../controllers/userController');
-
-const uploadDir = path.join(__dirname, '../uploads/face_enroll');
-fs.mkdirSync(uploadDir, { recursive: true });
-const uploadMemory = multer({ storage: multer.memoryStorage(), limits: { fileSize: 10 * 1024 * 1024 } });
+const { auth, checkRole } = require('../middleware/auth');
+const uploadPhoto = require('../middleware/uploadPhoto');
 
 const routerAPI = express.Router();
 
+routerAPI.all("*", auth) // Apply auth middleware to all routes in this router
 routerAPI.post('/login', handleLogin);
 routerAPI.post('/users/login', handleLogin); // alias cho client gửi theo mẫu /v1/api/users/login
-// routerAPI.get('/users', getUsersAPI);
-// routerAPI.post('/users', postCreateUserAPI);
-routerAPI.post('/users/enroll', uploadMemory.single('photo'), postEnrollUserAPI);
-// routerAPI.put('/users/:id', putUpdateUserAPI);
-// routerAPI.delete('/users/:id', deleteUserAPI);
+
+routerAPI.post('/users/enroll', checkRole(['ADMIN']), uploadPhoto.single('photo'), postEnrollUserAPI);
+
 
 module.exports = routerAPI;
